6bf2264e865f45113cff6e405fd5b7633faf220c
Lakshya Garg <lgarg7112@gmail.com>
2025-11-14 11:47:30 +0530

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+   int main() {
+   int A[3][3],x,y,B[3][3],C[3][3]; 
+   //user input in 2 arrays
+   for(x=0; x<3; x++){
+    for(y=0;y<3;y++){
+      cin >> A[x][y];
+    }
+   }
+   for(x=0; x<3; x++){
+    for(y=0;y<3;y++){
+      cin >> B[x][y];
+    }
+   }
+   //sum of matrix
+   for(x=0; x<3;x++){
+    for(y=0;y<3;y++){
+    C[x][y]=A[x][y]+B[x][y];
+      
+      }
+    }  
+   // display of sum 
+      for(x=0; x<3;x++){
+    for(y=0;y<3;y++){
+        cout<<C[x][y]<<" ";
+        
+    }  
+    cout<<endl;
+  }
+    return 0;
+
+}
---
+++ pattern.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void printPattern(int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
