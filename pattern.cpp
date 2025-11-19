8367c17612da1c4eafd4bdca323db37e5bce3d53
Lakshya Garg <lgarg7112@gmail.com>
2025-11-20 00:49:14 +0530

    pattern file

--- pattern.cpp
+++ pattern.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-using namespace std;
-void printPattern(int n) {
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "* ";
-        }
-        cout << endl;
+
+int main() {
+    int limit;
+    int a = 0, b = 1, nextTerm;
+
+    std::cout << "Enter the limit: ";
+    std::cin >> limit;
+
+    std::cout << "Fibonacci Series up to " << limit << ":\n";
+
+    if (limit >= 0) {
+        std::cout << a << " ";
     }
+    if (limit >= 1) {
+        std::cout << b << " ";
+    }
+
+    nextTerm = a + b;
+
+    while (nextTerm <= limit) {
+        std::cout << nextTerm << " ";
+        a = b;
+        b = nextTerm;
+        nextTerm = a + b;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
 }
