196142833452e710e90b108627d93b7443b593c0
Lakshya Garg <lgarg7112@gmail.com>
2025-11-19 20:14:41 +0530

    first commit

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
    int main() {
-   int A[3][3],x,y,B[3][3],C[3][3]; 
+   int A[3][3],x,y,B[3][3],C[3][3]={0}; 
    //user input in 2 arrays
    for(x=0; x<3; x++){
     for(y=0;y<3;y++){
@@ -18,7 +18,6 @@ using namespace std;
    for(x=0; x<3;x++){
     for(y=0;y<3;y++){
     C[x][y]=A[x][y]+B[x][y];
-      
       }
     }  
    // display of sum 
