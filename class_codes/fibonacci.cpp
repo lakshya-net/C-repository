d906272a8fb557bba980f5e33463b925386663bb
Lakshya Garg <lgarg7112@gmail.com>
2025-12-24 13:23:48 +0530

    new

---
+++ class_codes/fibonacci.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int fib(int t);
+int a = 0, b = 1, nextTerm , limit;
+int main() {
+    cout << "Enter the limit: ";
+    cin >> limit;
+    cout<<fib(limit);
+    return 0;
+}
+int fib(int t){
+
+    if (limit >= 0) {
+        cout << a << " ";
+    }
+    if (limit >= 1) {
+        cout << b << " ";
+    }
+
+    nextTerm = a + b;
+
+    while (nextTerm <= limit) {
+        cout << nextTerm << " ";
+        a = b;
+        b = nextTerm;
+        nextTerm = a + b;
+    }
+    return ; 
+}
+    
