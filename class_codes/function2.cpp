0985159c50d849636eaa0f1f697305f1ef25bd9e
Lakshya Garg <lgarg7112@gmail.com>
2025-12-26 23:48:07 +0530

    ..

--- class_codes/function2.cpp
+++ class_codes/function2.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <math.h>
 using namespace std;
+
+float areaAndPerimeter( int r, float *a, float *p) {
+    *a = M_PI * r * r;
+    *p = 2 *M_PI* r;
+    return 0;
+}
+
+
+int main() {
+    float radius, area, perimeter;
+    cin >> radius;
+
+    areaAndPerimeter(radius,&area,&perimeter);
+  cout << "Area: " << area << endl;
+  cout << "Perimeter: " << perimeter << endl;
+    return 0;
+}
