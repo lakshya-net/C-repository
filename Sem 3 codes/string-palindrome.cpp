1cd2de9621fc279bfa347a0b30f7b325a09cef47
Lakshya Garg <lgarg7112@gmail.com>
2026-07-27 22:55:30 +0530

    application

---
+++ Sem 3 codes/string-palindrome.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class stack{
+    
+    int top=-1;
+    int arr[1000];
+
+public:
+    
+
+    bool empty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    void push(int x){
+        if(top == 999){
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        top++;
+        arr[top] = x;
+    }
+
+    void pop(){
+        if(top == -1){
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        top--;
+    };
+
+    int peek(){
+        return arr[top];
+    };
+};
+
+int main (){
+    string str,ans="";
+    stack S;
+   cin>>str;
+    for(int i=0;i<str.length();i++){
+        S.push(str[i]);
+    }
+
+ while(!S.empty()){
+        ans+=S.peek();
+        S.pop();
+    }
+    if(ans==str){
+        cout<<"Palindrome"<<endl;
+    }
+    else{
+        cout<<"Not Palindrome"<<endl;
+    }
+    return 0;   
+}
