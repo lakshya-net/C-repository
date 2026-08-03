b4a0c1eb21b85ded45b23900ca69f8ef64b5fe3e
Lakshya Garg <lgarg7112@gmail.com>
2026-08-03 11:55:03 +0530

    ..

--- Sem 3 codes/test.cpp
+++ Sem 3 codes/test.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void show (int a){
-  cout<<a;
-}
-void show(int a,int b=10){
-  cout<<a+b;
-  }
-  int main(){
-    show(5);
+class student {
+    public:
+    string name;
+    int rollno;
+    float marks;
+    
+    void getdata()
+    {
+        cout<<"Enter name: ";
+        cin>>name;
+        cout<<"Enter roll number: ";
+        cin>>rollno;
+        cout<<"Enter marks: ";
+        cin>>marks;
     }
+    
+    void display()
+    {
+        cout<<"Name: "<<name<<endl;
+        cout<<"Roll Number: "<<rollno<<endl;
+        cout<<"Marks: "<<marks<<endl;
+    }
+};
+
+int main (){
+    student s1, s2, s3, s4, s5;
+    s1.getdata();
+    s2.getdata();
+    s3.getdata();
+    s4.getdata();
+    s5.getdata();
+
+    s1.display();
+    s2.display();
+    s3.display();
+    s4.display();
+    s5.display();
+    return 0;
+}
 
