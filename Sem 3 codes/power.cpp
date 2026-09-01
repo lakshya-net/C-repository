bd1893f03eff29ab27981e8c54579deab6e9eaa4
Lakshya Garg <lgarg7112@gmail.com>
2026-09-01 10:25:03 +0530

    /

---
+++ Sem 3 codes/power.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int pow(int base, int exponent){
+    if(exponent == 0){
+        return 1;
+    }
+    return base * pow(base, exponent - 1);
+}
+int main(){
+    cout<<pow(2,3);
+}
